868bf3506166e82f4803392edbc9b363fe1609a7
SrCastilloo <daniiterador@gmail.com>
2025-11-21 20:32:09 +0100

    Notificaciones v2.0

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,6 +1,16 @@
 // firebaseAdmin.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-service-account.json');
+
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  // En Render (y si quieres, también en local usando .env)
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  // Fallback para desarrollo local usando el archivo
+  // (este archivo NO se sube a GitHub porque está en .gitignore)
+  serviceAccount = require('./firebase-service-account.json');
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
